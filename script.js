2cfb4708478f5b1b178591f6f5cc20bfc85b4d47
Vlad <vladnovik123456789@gmail.com>
2024-05-04 23:17:49 +0200

    Picture animation added Footer and Header added Additional info added

---
+++ script.js
@@ -0,0 +1,7 @@
+let examplePic = document.querySelector(".example__pic")
+let section = 
+
+examplePic.addEventListener("click", function() {
+    examplePic.classList.toggle("picture_clicked")
+
+})
