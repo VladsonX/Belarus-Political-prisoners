98fff4d53fc3501c70d86d171ecc69778e889f66
Vlad <vladnovik123456789@gmail.com>
2024-05-12 14:14:44 +0200

    responsive media queries added

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 let examplePic = document.querySelector(".example__pic")
-let section = 
 
-examplePic.addEventListener("click", function() {
+examplePic.addEventListener("click", function () {
     examplePic.classList.toggle("picture_clicked")
 
+})
+
+let burger = document.querySelector("#burger");
+let header = document.querySelector("#header");
+let headerWrapper = document.querySelector(".header__wrapper");
+let burgerLine1 = document.querySelector(".burger__line_1");
+let burgerLine2 = document.querySelector(".burger__line_2");
+let burgerLine3 = document.querySelector(".burger__line_3");
+
+burger.addEventListener("click", function () {
+    header.classList.toggle("active-header");
+    headerWrapper.classList.toggle("active-header-wrapper");
+    burgerLine1.classList.toggle("active1");
+    burgerLine2.classList.toggle("active2");
+    burgerLine3.classList.toggle("active3");
 })
